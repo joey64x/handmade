38087b3f92e8ac943a9e3c8b5748a3f751ac42d3
Joey Lapegna <jlapegna@gmail.com>
2025-01-21 23:19:30 -0800

    day 03 complete - set up bitmap buffer ready to draw

--- src/win32_handmade.cpp
+++ src/win32_handmade.cpp
@@ -8,8 +8,56 @@
 
 #include <windows.h>
 
+#define internal static
+#define local_persist static
+#define global_variable static
+
+global_variable bool Running; // statics automatically intialize to zero
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void
+Win32ResizeDIBSection(int Width, int Height) // DIB = Device Independent Bitmap
+{
+    if (BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+    
+    if(!BitmapDeviceContext)
+    {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    BitmapHandle = CreateDIBSection(BitmapDeviceContext, &BitmapInfo,
+                                            DIB_RGB_COLORS,
+                                            &BitmapMemory,
+                                            0, 0); 
+}
+
+internal void
+Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    // this is a rectangle to rectangle copy - rects can be different sizes
+    StretchDIBits(DeviceContext,
+                  X, Y, Width, Height,
+                  X, Y, Width, Height,
+                  BitmapMemory,
+                  &BitmapInfo,
+                  DIB_RGB_COLORS, SRCCOPY);                  
+}
+
 LRESULT CALLBACK
-MainWindowCallback(HWND WindowHandle,
+Win32MainWindowCallback(HWND WindowHandle,
                    UINT Message,
                    WPARAM WParam,
                    LPARAM LParam)
@@ -20,17 +68,23 @@ MainWindowCallback(HWND WindowHandle,
     {
         case WM_SIZE:
         {
-            OutputDebugStringA("WM_SIZE\n");
+            RECT ClientRect;
+            GetClientRect(WindowHandle, &ClientRect);
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(Width, Height);
         } break;
 
         case WM_DESTROY:
         {
-            OutputDebugStringA("WM_DESTROY\n");
+            // TODO: Handle this as an error and recreate window?
+            Running = false;
         } break;
 
         case WM_CLOSE:
-        { 
-            OutputDebugStringA("WM_CLOSE\n");
+        {
+            // TODO: Handle this with a confirmation window?
+            Running = false;
         } break;
 
         case WM_ACTIVATEAPP:
@@ -49,7 +103,7 @@ MainWindowCallback(HWND WindowHandle,
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
 
-            PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
 
             EndPaint(WindowHandle, &Paint);
         } break;
@@ -71,7 +125,7 @@ int CALLBACK WinMain(HINSTANCE Instance,
     WNDCLASS WindowClass = {};
 	
     WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance;
 //      WindowClass.hIcon;
     WindowClass.lpszClassName = "HandmadeHeroWindowClass";
@@ -92,7 +146,9 @@ int CALLBACK WinMain(HINSTANCE Instance,
                                             0);
         if (WindowHandle)
         {
-            for(;;)
+            Running = true;
+
+            while(Running)
             {
                 MSG Message;
                 BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
